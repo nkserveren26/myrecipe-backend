ded181189291ba94196aae186bb7628e3be62bf1
nkserveren26 <norinoritoshi@outlook.jp>
2024-09-02 08:51:48 +0900

    Category Entityにgetterとsetterを追加

--- src/main/java/com/myrecipes/backend/entity/Category.java
+++ src/main/java/com/myrecipes/backend/entity/Category.java
@@ -14,4 +14,11 @@ public class Category {
     @Column(name = "name")
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
