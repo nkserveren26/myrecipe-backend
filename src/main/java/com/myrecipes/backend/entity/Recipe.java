2b3e6709b730b6a2ecfb7fad45ba8b04a029ea17
nkserveren26 <norinoritoshi@outlook.jp>
2024-09-02 08:50:42 +0900

    Recipe Entity作成

---
+++ src/main/java/com/myrecipes/backend/entity/Recipe.java
@@ -0,0 +1,81 @@
+package com.myrecipes.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recipe")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "category_id")
+    private int categoryId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "servings")
+    private int servings;
+
+    @Column(name = "image_path")
+    private String imagePath;
+
+    @Column(name = "video_url")
+    private String videoUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
