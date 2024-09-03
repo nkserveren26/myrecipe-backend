f40c13e0b563f0a2fbdd0c41873c2dda25d5160c
nkserveren26 <norinoritoshi@outlook.jp>
2024-09-04 08:36:30 +0900

    RecipeRestControllerのgetRecipesメソッド修正。

--- src/main/java/com/myrecipes/backend/rest/RecipeRestController.java
+++ src/main/java/com/myrecipes/backend/rest/RecipeRestController.java
@@ -1,15 +1,25 @@
 package com.myrecipes.backend.rest;
 
+import com.myrecipes.backend.entity.Recipe;
+import com.myrecipes.backend.service.RecipeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class RecipeRestController {
 
+    private RecipeService recipeService;
+
+    public RecipeRestController(RecipeService theRecipeService) {
+        recipeService = theRecipeService;
+    }
+
     @GetMapping("/recipes")
-    public String getRecipes() {
-        return "Recipes";
+    public List<Recipe> getRecipes() {
+        return recipeService.findAll();
     }
 }
