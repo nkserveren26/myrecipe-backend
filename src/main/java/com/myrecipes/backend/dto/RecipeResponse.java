66a2dd1c23af7d60e40960f3958e4a1f9593a3f1
nkserveren26 <norinoritoshi@outlook.jp>
2024-09-19 08:53:16 +0900

    RecipeResponseにGetterとSetterを設定。

--- src/main/java/com/myrecipes/backend/dto/RecipeResponse.java
+++ src/main/java/com/myrecipes/backend/dto/RecipeResponse.java
@@ -14,4 +14,36 @@ public class RecipeResponse {
         this.image = image;
         this.createdAt = createdAt;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
