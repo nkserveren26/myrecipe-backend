f7096d70ca2286b6ff27eb2adc763ced43406e69
nkserveren26 <norinoritoshi@outlook.jp>
2024-09-28 21:56:53 +0900

    レシピ追加APIの処理実装。Recipeインスタンスを新規作成、各フィールドに値をセット。

--- src/main/java/com/myrecipes/backend/rest/RecipeRestController.java
+++ src/main/java/com/myrecipes/backend/rest/RecipeRestController.java
@@ -8,6 +8,8 @@ import com.myrecipes.backend.service.RecipeService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @RestController
@@ -34,6 +36,28 @@ public class RecipeRestController {
     public void addRecipe(@RequestPart("recipe") AddRecipeRequestDTO addRecipeRequestDTO,@RequestPart("thumbnail") MultipartFile thumbnail) {
         System.out.println("Adding Recipe.");
         System.out.println(addRecipeRequestDTO);
+        System.out.println(thumbnail);
+
+        // Recipeインスタンス生成
+        Recipe recipe = new Recipe();
+
+        // Recipeインスタンスの各フィールドに値をセット
+        recipe.setTitle(addRecipeRequestDTO.getTitle());
+        recipe.setServings(addRecipeRequestDTO.getServings());
+        recipe.setVideoUrl(addRecipeRequestDTO.getVideoUrl());
+
+        // 日本時間の現在時刻を取得
+        ZoneId zoneId = ZoneId.of("Asia/Tokyo");
+        LocalDateTime jstNow = LocalDateTime.now(zoneId);
+
+        // createdAtフィールドに日本時間を設定
+        recipe.setCreatedAt(jstNow);
+
+        // 画像をS3にアップロード
+
+        // 署名付きURLをRecipeのimageフィールドにセット
+
+        // レシピを新規登録
     }
 
 }
