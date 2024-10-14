58acb0a118a21710d008b5c81443daf9c123a873
nkserveren26 <norinoritoshi@outlook.jp>
2024-10-14 22:00:51 +0900

    IngredientとStepのDTOにgetterとsetterを追加。

--- src/main/java/com/myrecipes/backend/dto/RecipeIngredientDTO.java
+++ src/main/java/com/myrecipes/backend/dto/RecipeIngredientDTO.java
@@ -8,4 +8,20 @@ public class RecipeIngredientDTO {
         this.name = name;
         this.amount = amount;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/com/myrecipes/backend/dto/RecipeStepDTO.java
+++ src/main/java/com/myrecipes/backend/dto/RecipeStepDTO.java
@@ -8,4 +8,20 @@ public class RecipeStepDTO {
         this.stepNumber = number;
         this.description = description;
     }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+
+    public void setStepNumber(int stepNumber) {
+        this.stepNumber = stepNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
