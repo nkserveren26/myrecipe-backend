5982887bde743e5c5ebd118301a1799bc9d4dc18
nkserveren26 <norinoritoshi@outlook.jp>
2024-11-27 21:12:04 +0900

    RecipeIngredientDAOImpl修正。findByRecipeId関数のクエリ文修正。

--- src/main/java/com/myrecipes/backend/dao/RecipeIngredientDAOImpl.java
+++ src/main/java/com/myrecipes/backend/dao/RecipeIngredientDAOImpl.java
@@ -20,7 +20,7 @@ public class RecipeIngredientDAOImpl implements RecipeIngredientDAO {
     }
     @Override
     public List<RecipeIngredient> findByRecipeId(int recipeId) {
-        String jpql = "SELECT ri FROM RecipeIngredient ri WHERE ri.recipeId = :recipeId";
+        String jpql = "SELECT ri FROM RecipeIngredient ri WHERE ri.recipe.id = :recipeId";
         TypedQuery<RecipeIngredient> query = entityManager.createQuery(jpql, RecipeIngredient.class);
         query.setParameter("recipeId", recipeId);
         return query.getResultList();
